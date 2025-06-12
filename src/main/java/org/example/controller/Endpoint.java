545c4e885b548ab8813db914f2c2209d29c9ebe7
vivek-011299 <vivek011299@gmail.com>
2025-06-12 23:34:14 +0530

    user input class and one api

--- src/main/java/org/example/controller/Endpoint.java
+++ src/main/java/org/example/controller/Endpoint.java
@@ -1,18 +1,17 @@
 package org.example.controller;
 
+import org.apache.coyote.Response;
+import org.example.model.UserInput;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Endpoint {
     @RequestMapping("/")
-    public String index()
+    public ResponseEntity<String> index()
     {
-        return "Index main page";
-    }
-    @RequestMapping("/hello")
-    public String hello()
-    {
-        return "Hello";
+        return ResponseEntity.ok("Data fetched from the user");
     }
 }
---
+++ src/main/java/org/example/model/UserInput.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+public class UserInput {
+    String input;
+    public void setInput(String input)
+    {
+        this.input = input;
+    }
+    public String getInput()
+    {
+        return this.input;
+    }
+}
